87f6a2cdca373ecbf4a671bff71fa0abe24ec0c9
Nguyễn Quốc Anh <168700063+qanh3007hcmut@users.noreply.github.com>
2025-07-24 09:50:24 +0700

    add cmt to contract

--- smart-contract/contracts/MyNFT.sol
+++ smart-contract/contracts/MyNFT.sol
@@ -6,11 +6,22 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/**
+ * @title Pokemon NFT Collection
+ * @author Nguyen Quoc Anh
+ * @notice NFT Smart Contract Core for Pokemon-themed digital collectibles
+ * @dev Extends ERC721 standard with custom minting permissions and URI handling
+ */
 contract MyNFT is ERC721, Ownable {
     uint256 public nextTokenId = 1;
     string internal baseTokenURI;
     mapping(address => bool) minter;
-
+    
+    /**
+     * @notice Initializes the contract with a base token URI
+     * @dev Sets up the ERC721 token with name "Pokemon" and symbol "PKM"
+     * @param _baseTokenURI The base URI for token metadata
+     */
     constructor(
         string memory _baseTokenURI
     ) ERC721("Pokemon", "PKM") Ownable(msg.sender) {
@@ -18,26 +29,47 @@ contract MyNFT is ERC721, Ownable {
         minter[msg.sender] = true;
     }
 
+    /**
+     * @notice Mints a new token to the specified address
+     * @dev Only authorized minters can call this function
+     * @param to The address that will receive the minted token
+     */
     function mint(address to) external {
         require(minter[msg.sender], "Unauthorized access");
         _safeMint(to, nextTokenId);
         nextTokenId++;
     }
 
+    /**
+     * @notice Returns the base URI for token metadata
+     * @dev Overrides the ERC721 _baseURI function
+     * @return The base URI string
+     */
     function _baseURI() internal view override returns (string memory) {
         return baseTokenURI;
     }
 
+    /**
+     * @notice Returns the URI for a given token ID
+     * @dev Appends ".json" to the token URI from the parent implementation
+     * @param tokenId The ID of the token to query
+     * @return The token URI string
+     */
     function tokenURI(
         uint256 tokenId
     ) public view override returns (string memory) {
         return string.concat(super.tokenURI(tokenId), ".json");
     }
 
+    /**
+     * @notice Updates the base URI for token metadata
+     * @dev Can only be called by the contract owner
+     * @param baseURI The new base URI to set
+     */
     function setBaseTokenURI(string memory baseURI) external onlyOwner {
         baseTokenURI = baseURI;
     }
-
+    
     function addMinter(address _minter) external onlyOwner {
         minter[_minter] = true;
     }
--- smart-contract/contracts/WhitelistForSale.sol
+++ smart-contract/contracts/WhitelistForSale.sol
@@ -6,11 +6,15 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
+/**
+ * @title Whitelist Sale for Pokemon NFT Collection
+ * @notice Manages whitelist-based minting of Pokemon NFTs with Merkle proof verification
+ * @dev Controls minting limits, supply cap, and whitelist access
+ */
 contract WhiteListSale is Ownable {
     using SafeERC20 for IERC20;
 
     MyNFT public nftContract;
-
     uint256 public _maxSupply = 1000;
     uint256 public _maxMintPerAddress = 5;
     bytes32 public _merkleRoot;
@@ -22,6 +26,10 @@ contract WhiteListSale is Ownable {
     event MerkleRootUpdated(bytes32 newMerkleRoot);
     event MintLimitUpdated(uint256 newMintLimit);
 
+    /**
+     * @notice Verifies sender is on the whitelist
+     * @param proofs Merkle proof for verification
+     */
     modifier onlyWhitelist(bytes32[] calldata proofs) {
         require(
             MerkleProof.verify(
@@ -42,55 +50,89 @@ contract WhiteListSale is Ownable {
         _;
     }
 
+    /**
+     * @notice Initializes the contract with NFT address and merkle root
+     * @param nftAddress Address of the NFT contract
+     * @param _root Initial merkle root for whitelist
+     */
     constructor(address nftAddress, bytes32 _root) Ownable(msg.sender) {
         nftContract = MyNFT(nftAddress);
         _merkleRoot = _root;
     }
 
+    /**
+     * @notice Mints an NFT if sender is whitelisted and within limits
+     * @param proofs Merkle proof verifying sender is whitelisted
+     */
     function mintNFT(
         bytes32[] calldata proofs
     ) external onlyWhitelist(proofs) withinLimit {
         require(nftContract.nextTokenId() <= _maxSupply, "Max supply reached");
         _mintPerAddr[msg.sender] += 1;
         nftContract.mint(msg.sender);
-        emit TokenMinted(msg.sender, nftContract.nextTokenId() -1);
+        emit TokenMinted(msg.sender, nftContract.nextTokenId() - 1);
     }
 
+    /**
+     * @notice Withdraws all ETH from contract to owner
+     */
     function withdraw() external onlyOwner {
         require(address(this).balance > 0, "No balance to withdraw");
         (bool success, ) = msg.sender.call{value: address(this).balance}("");
         require(success, "Transfer failed");
     }
 
+    /**
+     * @notice Withdraws ERC20 tokens from contract to owner
+     * @param token Address of ERC20 token to withdraw
+     */
     function withdrawToken(address token) external onlyOwner {
         uint256 balance = IERC20(token).balanceOf(address(this));
         require(balance > 0, "No token balance to withdraw");
         IERC20(token).safeTransfer(msg.sender, balance);
     }
 
+    /**
+     * @notice Returns current total supply of minted NFTs
+     * @return Current number of minted NFTs
+     */
     function _getTotalSupply() external view returns (uint256) {
         return nftContract.nextTokenId() - 1;
     }
 
+    /**
+     * @notice Updates maximum supply cap
+     * @param maxSupply New maximum supply value
+     */
     function setMaxSupply(uint256 maxSupply) external onlyOwner {
         require(maxSupply > 0, "Max supply must be greater than zero");
         _maxSupply = maxSupply;
         emit MaxSupplyUpdated(_maxSupply);
     }
 
+    /**
+     * @notice Updates maximum mint limit per address
+     * @param newLimit New mint limit per address
+     */
     function setMaxMintPerAddresss(uint256 newLimit) external onlyOwner {
         require(newLimit > 0, "Mint Limit must be greater than zero");
         _maxMintPerAddress = newLimit;
         emit MintLimitUpdated(_maxMintPerAddress);
     }
 
+    /**
+     * @notice Updates merkle root for whitelist verification
+     * @param newMerkleRoot New merkle root hash
+     */
     function setMerkleRoot(bytes32 newMerkleRoot) public onlyOwner {
         require(newMerkleRoot != bytes32(0), "Merkle Root cannot be 0");
         _merkleRoot = newMerkleRoot;
         emit MerkleRootUpdated(_merkleRoot);
     }
 
+    /// @notice Allows contract to receive ETH
     receive() external payable {}
 
+    /// @notice Fallback function to receive ETH
     fallback() external payable {}
 }
